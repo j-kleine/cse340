170e7eab9d23f8a9a7d3213c13b828e8c8792083
j-kleine <joschuadkleine@gmail.com>
2024-03-21 02:02:22 +0100

    W04 LA 1.4 minor adjustment

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -46,7 +46,7 @@ async function registerAccount(req, res) {
   if (regResult) {
     req.flash(
       "notice",
-      `Congratulations, you're registered ${account_firstname}. Please log in.`
+      `Congratulations, you're registered, ${account_firstname}. Please log in.`
     )
     res.status(201).render("account/login", {
       title: "Login",
